e1195677bf98ce43ae30a5aef87094c5d0d2c49d
arinavis <avisnevskaja@gmail.com>
2024-09-23 20:07:26 +0300

    hi

---
+++ main.py
@@ -0,0 +1,283 @@
+from ctypes.wintypes import HICON
+
+print("labas")
+# 1 uzd.
+print(15)
+print(12)
+print(89)
+print(5.8)
+# 2 uzd.
+print(78*2)
+print(5+3)
+print(7/2)
+print(85-32)
+# 3 uzd.
+print(5.2*3)
+print(9.4*0.5)
+print(4.2/2)
+# 4 uzd.
+print(7+2*3)
+print((7+2)*3)
+print(52+74+32)
+print(87-65+14)
+print(79/(5-2))
+# 5 uzduotis
+print(2**4)
+print(8**3)
+print(14**0.5)
+# 6 uzd.
+print(2%2)
+print(3%2)
+print(11%2)
+print(13%2)
+print(10%2)
+# 7 uzd.
+print(15%2)
+print(15%3)
+print(15%4)
+print(15%5)
+print(15%6)
+print(15%7)
+print(15%8)
+print(15%9)
+# 8 uzd.
+print(5 // 2)
+print(6 // 3)
+print(6 // 4)
+print(80 // 3)
+print(45 // 4)
+print(45 // 3)
+# 9 uzd.
+print(type(6))
+print(type(2.5))
+print(type(157))
+print(type(-8.2))
+print(type(2 / 5))
+print(type(8 + 9))
+print(type(8.5 + 3))
+
+# Kintamieji
+# 1 uzd.
+Vardas = 'Jonas'
+Pavarde = 'Petraitis'
+Amzius = 20
+Ugis = 190
+Svoris = 90
+Aukstoji_mokykla = 'Vilniaus kolegija'
+Ak_gr_kodas = 1234
+Kursas = 2
+St_prgr_pav = 'Informatika'
+Ats_krd_sk = 50
+
+print(Vardas)
+print(Pavarde)
+print(Amzius)
+print(Ugis)
+print(Svoris)
+print(Aukstoji_mokykla)
+print(Ak_gr_kodas)
+print(Kursas)
+print(St_prgr_pav)
+print(Ats_krd_sk)
+print()
+# 2 uzd
+pavadinimas = 'Vilnius'
+valstybe = 'Lietuva'
+apskritis = 'Vilniaus'
+ikurimo_data = 1323
+meras = 'Benkunskas'
+plotas = 401
+gyv_sk = 600
+
+print()
+print(pavadinimas)
+print(valstybe)
+print(apskritis)
+print(ikurimo_data, 'metai')
+print(meras)
+print(plotas, 'kv.km')
+print(gyv_sk, 'tukst')
+# 3 uzd.
+print()
+vardas = 'Arina'
+
+print("Mano vardas yra", vardas)
+
+print()
+ak_gr = 'Inf-10'
+vidurkis = 9
+
+print("Akademinė grupė:", ak_gr)
+print("Vidurkis:", vidurkis)
+
+print()
+zodis = 'Hello'
+
+# informacijos išvedimas
+# 1 uzd.
+print(zodis, zodis, zodis, zodis, zodis)
+print()
+vardas = 'Arina'
+amzius = 28
+programavimas = 'nes įdomu'
+
+print(vardas, ',', '(' + str(amzius) + ' metai)')
+print(f'{vardas}, ({amzius}  metai)')
+print('pasirinkau progrmavimą,', programavimas)
+
+print()
+# 2 PASKAITA. 2024-09-19
+
+# 2 uzd.
+print('Koks baltumas, neapsakomas baltumas!\nKur dangus, kur žemė – neatskirsi.\nKrinta sniegas – baltas nerūpestingumas.\nRodos, šūktelsi iš džiaugsmo ir numirsi.')
+
+print()
+# 3 uzd.
+
+print()
+# # Duomenų įvedimas
+# # 1
+# print('kuo tu vardu')
+# vardas = input()
+# print(f'labas {vardas}')
+#
+# # 2
+# vardas = input('kuo tu vardu?')
+# print(f'labas {vardas}')
+
+# print('Dviejų sveikųjų skaičių sumavimo skaičiuotuvas')
+# print('pirmas skaičius:')
+# sk1 = int( input() )
+# print('antras skaičius')
+# sk2 = int( input() )
+# print(f'{sk1} + {sk2} = {sk1 + sk2}')
+
+# Aritmetiniai veiksmai
+# 2 užd.
+Sk1 = 5
+Sk2 = 10
+Sk3 = Sk1 + Sk2
+
+print(Sk1)
+print(Sk2)
+print(Sk3)
+
+print()
+#3 uzd
+
+sk_1 = 1
+sk_2 = 2
+sk_3 = 3
+
+print((f'{sk_1} + {sk_2} + {sk_3} ='), sk_1 + sk_2 + sk_3)
+print((f'{sk_1} - {sk_2} - {sk_3} ='), sk_1 - sk_2 - sk_3)
+print((f'{sk_1} * {sk_2} * {sk_3} ='), sk_1 * sk_2 * sk_3)
+print((f'{sk_1} / {sk_2} / {sk_3} ='), sk_1 / sk_2 / sk_3)
+# 4 uzd
+
+print()
+sk1 = 7
+sk2 = 8
+
+sk11 = sk1 + 5
+sk22 = sk2 * 2
+
+print(sk1)
+print(sk2)
+
+print(sk11)
+print(sk22)
+
+print()
+#input
+#1 uzd
+# print('Kuo tu vardu?')
+# Vardas = input()
+# print(f'Mano vardas {Vardas}')
+#
+# print('Kiek tau metų?')
+# Amzius = int( input() )
+# print(f'Man {Amzius} metai')
+#
+# print('Kodėl pasirinkai progrmavimą?')
+# Pasirinkimas = input()
+# print(f'Todėl, kad {Pasirinkimas}')
+
+# 2 uzd.
+# print('Įveskite simbolį')
+# simbolis = input()
+# print(simbolis * 3)
+# print(simbolis, simbolis)
+# print(simbolis * 3)
+
+# 4 uzd
+# vardas = str( input('vartotojo vardas:'))
+# print(vardas, type(vardas))
+# amzius = int( input('vartotojo amžius:'))
+# print(amzius, type(amzius))
+# ugis = float( input('vartotojo ūgis:'))
+# print(ugis, type(ugis))
+
+# 5 uzd.
+# pazymis1 = int( input('Įveskite pažymį:') )
+# pazymis2 = int( input('Įveskite pažymį:') )
+# pazymis3 = int( input('Įveskite pažymį:') )
+# pazymis4 = int( input('Įveskite pažymį:') )
+# pazymis5 = int( input('Įveskite pažymį:') )
+# print('Jūsų vidurkis:', (pazymis1 + pazymis2 + pazymis3 + pazymis4 + pazymis5)/5)
+
+# 8 uzd.
+# sk1 = int( input('Įveskite skaičių:'))
+# sk2 = int( input('Įveskite skaičių:'))
+# print('Liekana:', sk1 % sk2)
+
+# 9 užd
+# sk_pagr = int( input('Įveskite skaičių:') )
+# sk_laipsnis = int( input('Įveskite skaičių:') )
+# print('Pakelta laipsniu:', sk_pagr ** sk_laipsnis)
+
+# Sąlygos
+if 5 < 10:
+    print('5 yra mažiau už 10')
+
+# 1 uzd.
+
+skaicius1 = 5
+skaicius2 = 8
+skaicius3 = 24
+
+# 1.1
+if skaicius2 == skaicius3:
+    print('true')
+print()
+# 1.2
+if skaicius1 > skaicius2:
+    print('true')
+print()
+# 1.3
+if skaicius2 > skaicius3 * 2:
+    print('true')
+print()
+# 1.4
+if skaicius1 % 2 == 0:
+    print('true')
+print()
+# 1.5
+if skaicius2 % 2 == 0:
+    print('lyginis')
+else: print('nelyginis')
+print()
+# 1.6
+if skaicius3 > 0:
+    print('sk. teigiamas')
+print()
+# 1.7
+if skaicius1 < 0:
+    print('neigiamas')
+# 1.8
+if skaicius2 % 4 == 0:
+    print('true')
+print()
+
+# git config --global user.name "arinavis"
+# git config --global user.email "avisnevskaja@gmail.com"
